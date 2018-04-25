2b89e78ceae343aed0cb77be988d40151d91cf74
Soumya Dey <24648@ITCINFOTECH.COM>
2018-04-25 19:39:31 +0530

    Journey Log API v1.1

--- index.js
+++ index.js
@@ -1,39 +1,81 @@
 const express= require("express");
 const helmet= require("helmet");
+const bodyParser= require('body-parser')
 const compression= require("compression");
+const mongoose= require('mongoose');
+
+// To handle the mongoose promise deprecation
+mongoose.Promise= require("bluebird");
+
+mongoose.connect(`mongodb://admin:${encodeURIComponent('super@admin')}@ds014578.mlab.com:14578/voyage_report`)
+        .then(()=> console.log("Scccessfully Connected to MongoDB Server of MLAB..."))
+        .catch((error)=> console.log("Cannot connect to MongoDB Server of MLAB...", error));
+
 const app= express();
-
+// app.use(bodyParser, {urlEncoded: true});
 app.use(express.json());
-// app.use(express.helmet());
-// app.use(express.compression())
 
-const port= process.env.PORT| 3000;
+const port= process.env.PORT | 3000;
+
+const reportSchema={
+    reportType: String,
+    report: Array
+}
+const Report= mongoose.model('Report', reportSchema);
+const REPORT_ID='5ae07c084a2c99340418b258';
 
 //Get request to resource http://localhost:3000
 app.get('/', (req, res)=>{
-    res.send("Welcome to our API")
+    res.send("Welcome to our API");
 });
 
 //GET the Current Voyagwe Report from DB
 app.get('/api/journeyReports', (req, res)=>{
     // call to database and fetch voyage report
-    let report= {};
-    res.status(200).send(report);
+   getReport(res);
 });
 
 //POST the commander record to DB
 app.post('/api/journeyReports', (req, res)=>{
-    let curretReport= req.body;
-    //Update the DB record with current report
-        let acknowledgement={
-            "ReportId": 1,
-            "SuccessMeggae": "Successfully entered the report",
-            "Report": curretReport
-        }
-    console.log("Report Accepted")
-    //Send the acknowledgement message to user
-    return res.status(200).send(acknowledgement);
+    // Fetching only the report obj from request body
+    let bodyObj = req.body.report;
+    queryAndUpdateCourse(bodyObj, res);
+});
 
-})
+//Fetch Report from MongoDB
+async function getReport(res, reportId){
+    // let report= await Report.findById(reportId);
+    let report= await Report.find({});
+    if(!report)
+        return null;
+    console.log(`Fetched Report: ${report}`)
+    res.status(200).send(report);
+}
+
+//query and course and then update the properties
+async function queryAndUpdateCourse(reqBody, res){
+    const updatedRecord = await Report.findByIdAndUpdate(
+                            {_id:REPORT_ID},
+                            {
+                                $set: {
+                                    report: reqBody // Report updated here
+                                },
+                            
+                            }, {
+                                new: true
+                            });
+    res.status(200).send(updatedRecord);
+}
 
 app.listen(port, ()=>console.log(`Server running on http://localhost:${port}`))
+
+//Create a Report
+// async function createReport(res, reportObj){
+//     let reportObject= new Report({
+//         reportType: "Voyage Report",
+//         report: reportObj
+//     })  
+//   let resultObj= await reportObject.save();
+//   console.log(`Created Report: ${resultObj}`)
+//     res.status(200).send(resultObj);
+// }
