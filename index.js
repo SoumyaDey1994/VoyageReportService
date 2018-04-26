e69b36ca26c8eb76ca026e5085832746a86a563d
Soumya Dey <24648@ITCINFOTECH.COM>
2018-04-26 16:08:49 +0530

    Journey Log API v1.4

--- index.js
+++ index.js
@@ -1,22 +1,31 @@
 const express= require("express");
-const helmet= require("helmet");
-const bodyParser= require('body-parser')
-const compression= require("compression");
 const mongoose= require('mongoose');
+mongoose.Promise= require("bluebird");  // To handle the mongoose promise deprecation
+const joi= require('joi');
 
-// To handle the mongoose promise deprecation
-mongoose.Promise= require("bluebird");
-
+//connect to MongoDb database on MLAB
 mongoose.connect(`mongodb://admin:${encodeURIComponent('super@admin')}@ds014578.mlab.com:14578/voyage_report`)
         .then(()=> console.log("Scccessfully Connected to MongoDB Server of MLAB..."))
         .catch((error)=> console.log("Cannot connect to MongoDB Server of MLAB...", error));
 
+//Create Instance of Express app
 const app= express();
-// app.use(bodyParser, {urlEncoded: true});
+//Parse request and response object as JSON
 app.use(express.json());
 
 const port= process.env.PORT || 3000;
 
+//statement object schema 
+const statementSchema=joi.object().keys({
+    statementNo: joi.number().required(),
+    statement: joi.string().required()
+})
+
+//schema to validate user submitted report
+const submittedJourneyReportSchema={
+    report: joi.array().items(statementSchema).min(1).required()
+}
+//Schema to store journey report to MongoDB Collection
 const reportSchema={
     reportType: String,
     report: Array
@@ -26,7 +35,7 @@ const REPORT_ID='5ae07c084a2c99340418b258';
 
 //Get request to resource http://localhost:3000
 app.get('/', (req, res)=>{
-    res.send("Welcome to our API");
+    res.sendFile('public/index.html', {root: __dirname});
 });
 
 //GET the Current Voyagwe Report from DB
@@ -38,22 +47,28 @@ app.get('/api/journeyReports', (req, res)=>{
 //POST the commander record to DB
 app.post('/api/journeyReports', (req, res)=>{
     // Fetching only the report obj from request body
-    let bodyObj = req.body.report;
-    queryAndUpdateCourse(bodyObj, res);
+    let requestBody = req.body;
+    let validationResult= joi.validate(requestBody, submittedJourneyReportSchema)
+    if(validationResult.error)
+        return res.status(400).send("Error: "+validationResult.error.details[0].message);
+    
+    let report= requestBody.report;
+    updateTheReport(report, res);
 });
 
 //Fetch Report from MongoDB
-async function getReport(res, reportId){
+async function getReport(res){
     // let report= await Report.findById(reportId);
-    let report= await Report.find({});
+    let report= await Report.findById(REPORT_ID);
     if(!report)
-        return null;
+        return res.status(404).send(`The Requested Report not available`);
+
     console.log(`Fetched Report: ${report}`)
     res.status(200).send(report);
 }
 
 //query and course and then update the properties
-async function queryAndUpdateCourse(reqBody, res){
+async function updateTheReport(reqBody, res){
     const updatedRecord = await Report.findByIdAndUpdate(
                             {_id:REPORT_ID},
                             {
